17935c22d4f70260182e1b8a2a0be1e686fb79e3
Kelly Rauchenberger <fefferburbia@gmail.com>
2018-03-25 18:02:37 -0400

    Whitespace changes

--- manifesto.cpp
+++ manifesto.cpp
@@ -15,26 +15,26 @@ int main(int argc, char** argv)
 
   std::string configfile(argv[1]);
   YAML::Node config = YAML::LoadFile(configfile);
-    
+
   twitter::auth auth;
   auth.setConsumerKey(config["consumer_key"].as<std::string>());
   auth.setConsumerSecret(config["consumer_secret"].as<std::string>());
   auth.setAccessKey(config["access_key"].as<std::string>());
   auth.setAccessSecret(config["access_secret"].as<std::string>());
-  
+
   twitter::client client(auth);
-  
+
   long loc = 0;
   if (config["current_location"])
   {
     loc = config["current_location"].as<long>();
   }
-  
+
   std::list<std::string> tweets;
   {
     std::ifstream corpus(config["corpus"].as<std::string>());
     corpus.ignore(loc);
-  
+
     char buffer[140];
     while (corpus)
     {
@@ -42,14 +42,14 @@ int main(int argc, char** argv)
       tweets.push_back(std::string(buffer, 140));
     }
   }
-  
+
   while (!tweets.empty())
   {
     try
     {
       std::string nextTweet = tweets.front();
       client.updateStatus(nextTweet);
-      
+
       std::cout << "Tweeted!" << std::endl;
 
       loc += 140;
@@ -64,9 +64,9 @@ int main(int argc, char** argv)
     {
       std::cout << "Twitter error: " << e.what() << std::endl;
     }
-    
+
     std::cout << "Waiting..." << std::endl;
-    
+
     std::this_thread::sleep_for(std::chrono::hours(6));
   }
 }
